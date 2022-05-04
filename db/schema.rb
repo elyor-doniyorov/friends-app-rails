ea64578f7c83e424f0f8f7d64972c6b087b1945b
elyor-doniyorov <doniyorovelyor@gmail.com>
2022-05-05 02:06:17 +0500

    Create friend model and add links

---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Schema.define(version: 2022_05_04_205032) do
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
